8038b025550f5652b60ed2f93996f5dbc17b2db1
Christian Nagel <christian@christiannagel.com>
2017-11-20 11:04:11 +0100

    dependency injection sample

---
+++ DISample/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DISample
+{
+    public class HomeController
+    {
+        private readonly IGreetingService _greetingService;
+        public HomeController(IGreetingService greetingService)
+        {
+            _greetingService = greetingService;
+        }
+
+        public string Hello(string name)
+        {
+            // var service = new GreetingService();
+            //  string message = service.Greet(name);
+            string message = _greetingService.Greet(name);
+            return message;
+        }
+    }
+}
