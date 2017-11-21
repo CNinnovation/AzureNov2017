896f807a69b0a7fffb89b45a85730e01531b8d9a
Christian Nagel <christian@christiannagel.com>
2017-11-21 12:56:43 +0100

    web job

---
+++ ASPNETCoreSample/SampleWebJob/Functions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace SampleWebJob
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ProcessQueueMessage([TimerTrigger("0 0 */24 * * *", RunOnStartup = true)] TimerInfo timerInfo, TextWriter log)
+        {
+            log.WriteLine("processing...");
+        }
+    }
+}
