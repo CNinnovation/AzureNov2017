519ccbaff91fb83942aad7f9bd8b2a143cd3f292
Christian Nagel <christian@christiannagel.com>
2017-11-21 12:56:17 +0100

    API sample

---
+++ MyAPISample/BooksLib/Controllers/BooksController.cs
@@ -0,0 +1,52 @@
+﻿using BooksLib.Models;
+using BooksLib.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BooksLib.Controllers
+{
+    [Route("api/[controller]")]
+    public class BooksController : Controller
+    {
+        private readonly IBooksService _booksService;
+        public BooksController(IBooksService booksService)
+        {
+            _booksService = booksService;
+        }
+       
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Book> GetBooks()
+        {
+            return _booksService.GetAllBooks();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]Book value)
+        {
+            _booksService.AddBook(value);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
