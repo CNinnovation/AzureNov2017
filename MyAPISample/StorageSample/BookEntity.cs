01ea267be540e1b356db59b7cd3d3adf3fa28f9d
Christian Nagel <christian@christiannagel.com>
2017-11-23 13:08:46 +0100

    storage sample

---
+++ MyAPISample/StorageSample/BookEntity.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StorageSample
+{
+    public class BookEntity : TableEntity
+    {
+        public BookEntity()
+        {
+
+        }
+
+        public BookEntity(string partitionKey, string rowKey)
+            : base(partitionKey, rowKey)
+        {
+
+        }
+
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+    }
+}
---
+++ MyAPISample/StorageSample/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Threading.Tasks;
+
+namespace StorageSample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await TableStorageSampleAsync();
+        }
+
+        private static async Task TableStorageSampleAsync()
+        {
+            CloudStorageAccount account = CloudStorageAccount.Parse("use your azure storage connection");
+            CloudTableClient tableClient = account.CreateCloudTableClient();
+            CloudTable booksTable = tableClient.GetTableReference("books");
+            bool created = await booksTable.CreateIfNotExistsAsync();
+            BookEntity myBook = new BookEntity("wroxpress", Guid.NewGuid().ToString())
+            {
+                Title = "Professional C# 6",
+                Publisher = "Wrox Press"
+            };
+            TableOperation insertOperation = TableOperation.Insert(myBook);
+            TableResult result = await booksTable.ExecuteAsync(insertOperation);
+        }
+    }
+}
